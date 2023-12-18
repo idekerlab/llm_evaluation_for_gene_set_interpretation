cbcb0c897a41bbb0b9b0c94e14c2c8290e6bd80a
Ingoo Lee <i5lee@nrnb-login-01.cm.cluster>
2023-12-18 13:00:06 -0800

    Reference checking fixing

--- utils/reference_checker.py
+++ utils/reference_checker.py
@@ -4,6 +4,7 @@ import requests
 from utils.openai_query import openai_chat
 import os 
 import json
+import re
 
 
 def get_genes_from_paragraph(paragraph, config, verbose=False):
@@ -76,11 +77,13 @@ def get_keywords_combinations(paragraph, config, verbose=False):
     gene_query_title = " OR ".join(["(%s[Title])"%gene for gene in genes])
     keywords_title = [gene_query_title + " AND (%s[Title])"%function for function in functions]
     
-    gene_query = " OR ".join(["(%s[Title/Abstract])"%gene for gene in genes])
-    keywords = [gene_query + " AND (%s[Title/Abstract])"%function for function in functions]
-    keywords = keywords_title + keywords
+    gene_query = " OR ".join(['"%s"'%gene for gene in genes])
+    function_query = " OR ".join(['"%s"'%function for function in functions])
+    #keywords = [gene_query + " AND (%s[Title/Abstract])"%function for function in functions]
+    #keywords = keywords_title + keywords
+    keywords = "(%s) AND (%s) AND (hasabstract[text])"%(gene_query, function_query)
 
-    return keywords, False # SA modified
+    return keywords, genes, functions, False # SA modified
     
 def get_mla_citation(doi):
     url = f'https://api.crossref.org/works/{doi}'
@@ -112,22 +115,32 @@ def get_mla_citation(doi):
         mla_citation += f", {year}, pp. {page}."
         
         return mla_citation
-    
+
+## 12/18/2023 IL updated the following main function
 def get_mla_citation_from_pubmed_id(paper_dict):
     article = paper_dict['MedlineCitation']['Article']
     #print(article.keys())
     authors = article['AuthorList']
     formatted_authors = []
     for author in authors:
-        last_name = author['LastName'] if author['LastName'] is not None else ''
-        first_name = author['ForeName'] if author['ForeName'] is not None else ''
+        try:
+            last_name = author['LastName'] if author['LastName'] is not None else ''
+        except:
+            last_name = ''
+        try:
+            first_name = author['ForeName'] if author['ForeName'] is not None else ''
+        except:
+            first_name = ''
         formatted_authors.append(f"{last_name}, {first_name}")
     authors_str = ', '.join(formatted_authors)
 
     title = article['ArticleTitle']
     journal = article['Journal']['Title']
     year = article['Journal']['JournalIssue']['PubDate']['Year']
-    page = article['Pagination']['MedlinePgn']
+    try:
+        page = article['Pagination']['MedlinePgn']
+    except:
+        page = " "
     mla_citation = f"{authors_str}. \"{title}\" {journal}"
     if "Volume" in article['Journal']['JournalIssue']['PubDate']:
         volume = article['Journal']['JournalIssue']['PubDate']['Volume']
@@ -154,6 +167,120 @@ def get_citation(paper):
         pages = doi.strip().split(".")[-1]
     citation = f"{names}. {corrected_title} {journal} {volume} ({pub_date[0:4]}):{pages}"
     return citation
+
+## 12/18/2023 IL updated the following main function
+def check_title_matching(paper, paragraph, config, n=10, verbose=False):
+    ## load config for openai query
+    context = config['CONTEXT']
+    gpt_model = config['GPT_MODEL']
+    temperature = config['TEMP']
+    max_tokens = config['MAX_TOKENS']
+    rate_per_token = config['RATE_PER_TOKEN']
+    LOG_FILE = config['LOG_NAME']+'log.json'
+    DOLLAR_LIMIT = config['DOLLAR_LIMIT']
+    
+    try:
+        title = paper['MedlineCitation']['Article']['ArticleTitle']
+    except (KeyError, IndexError) as e:
+        if verbose:
+            print("Error in getting abstract from paper.")
+            print("Error detail: ", e)
+        return False
+    message = f"""
+I have a paragraph
+Paragraph:
+{paragraph}
+
+and an scientific paper title.
+Abstract:
+{title}
+
+Does this title support paragraph? Please tell me yes or no
+"""    
+    try:
+        result = openai_chat(context, message, gpt_model, temperature, max_tokens, rate_per_token, LOG_FILE, DOLLAR_LIMIT)
+    except Exception as e:
+        print("Error in openai_chat")
+        print("Error detail: ", e)
+        result = None
+        return False
+    if result[:3].lower()=='yes':
+        return True
+    else:
+        return False
+
+## 12/18/2023 IL updated the following main function
+def check_abstract_match(paper, paragraph, config, verbose=False):
+    ## load config for openai query
+    context = config['CONTEXT']
+    gpt_model = config['GPT_MODEL']
+    temperature = config['TEMP']
+    max_tokens = config['MAX_TOKENS']
+    rate_per_token = config['RATE_PER_TOKEN']
+    LOG_FILE = config['LOG_NAME']+'log.json'
+    DOLLAR_LIMIT = config['DOLLAR_LIMIT']
+
+    try:
+        abstract = "\n".join(paper['MedlineCitation']['Article']['Abstract']['AbstractText'])
+    except (KeyError, IndexError) as e:
+        if verbose:
+            print("Error in getting abstract from paper.")
+            print("Error detail: ", e)
+        return False
+    message = f"""
+I have a paragraph
+Paragraph:
+{paragraph}
+
+and an abstract.
+Abstract:
+{abstract}
+
+Does this abstract support paragraph? Please tell me yes or no
+        """
+        
+    try:
+        result = openai_chat(context, message, gpt_model, temperature, max_tokens, rate_per_token, LOG_FILE, DOLLAR_LIMIT)
+    except Exception as e:
+        print("Error in openai_chat")
+        print("Error detail: ", e)
+        result = None
+    if result is not None:
+        if verbose:
+            print("Query: ", message)
+            print("Result: ", result)
+        if result[:3].lower()=='yes':
+            return True
+        else:
+            return False
+
+## 12/18/2023 IL updated the following main function
+def get_genes_in_abstract(paper, genes, verbose=False):
+    ## load config for openai query
+    try:
+        title = paper['MedlineCitation']['Article']['ArticleTitle']
+        abstract = paper['MedlineCitation']['Article']['Abstract']['AbstractText'][0]
+    except (KeyError, IndexError) as e:
+        if verbose:
+            print("Error in getting abstract from paper.")
+            print("Error detail: ", e)
+        return 0
+    gene_counts = 0
+    genes_in_abstract = []
+    for gene in genes:
+        if gene.lower() in abstract.lower():
+            gene_counts += 1
+            genes_in_abstract.append(gene)
+    if verbose:
+        print("Title: ", title)
+        print("Abstract: ", abstract)
+        print("Genes: ", ", ".join(genes_in_abstract))
+        print(" ")
+    return genes_in_abstract
+
+## 12/18/2023 IL updated the following main function
+def sort_paper_by_n_genes_in_abstract(papers, genes, verbose=False):
+    return list(reversed(sorted(papers, key=lambda paper: len(get_genes_in_abstract(paper, genes, verbose=verbose)))))
 
 def get_references(queried_papers, paragraph, config, n=10, verbose=False):
     ## load config for openai query
@@ -241,19 +368,19 @@ def search_pubmed(keywords, email, sort_by='relevance', retmax=10): ### CH: sort
 
 def get_papers(keywords, n, email):
     total_papers = []
-    for keyword in keywords:
-        print("Searching Keyword :", keyword)
-        try:
-            pubmed_queried_keywords= search_pubmed(keyword, email=email)
-            print("%d papers are found"%len(pubmed_queried_keywords))
-            total_papers += list(pubmed_queried_keywords[:n])
-            
-        except:
-            print("No paper found")
-            pass
+    #for keyword in keywords:
+    print("Searching Keyword :", keywords)
+    try:
+        pubmed_queried_keywords= search_pubmed(keywords, email=email)
+        print("%d papers are found"%len(pubmed_queried_keywords))
+        total_papers += list(pubmed_queried_keywords[:n])
+
+    except:
+        print("No paper found")
+        pass
     return total_papers
 
-## 06/12/2023 CH updated the following main function
+## 12/18/2023 IL updated the following main function
 def get_references_for_paragraphs(paragraphs, email, config, n=5, verbose=False, MarkedParagraphs=[], saveto = 'paragraph_ref_data'):
     '''
     paragraphs: list of paragraphs
@@ -267,25 +394,62 @@ def get_references_for_paragraphs(paragraphs, email, config, n=5, verbose=False,
     
     references_paragraphs = []
     paragraph_data = {}
+    abstracts = []
     for i, paragraph in enumerate(paragraphs):
         if verbose:
             print("""Extracting keywords from paragraph\nParagraph:\n%s"""%paragraph)
             print("="*75)
-        keywords, flag_working = get_keywords_combinations(paragraph, config = config, verbose=verbose) # SA: modified
+        keywords, genes, functions, flag_working = get_keywords_combinations(paragraph, config=config, verbose=verbose) # SA: modified
+        if verbose:
+            print("PubMed Keywords: ", keywords)
         if flag_working: # collect genes or functions keywords return None and come back later 
             MarkedParagraphs.append((i,paragraph))
         #keywords = list(sorted(keywords, key=len))
         # keyword_joined = ",".join(keywords)
         # print("Keywords: ", keyword_joined)
         print("Serching paper with keywords...")
-        pubmed_queried_keywords= get_papers(keywords, n, email)
-
-        print("In paragraph %d, %d references are queried"%(i+1, len(pubmed_queried_keywords)))
+        papers = search_pubmed(keywords, email, retmax=100)
         
-        if len(pubmed_queried_keywords)==0:
+        print("In paragraph %d, %d references are queried"%(i+1, len(papers)))
+        
+        if len(papers)==0:
             print("No paper searched!!")
-        references = get_references(pubmed_queried_keywords, paragraph, config = config, n=n, verbose=verbose)
+        sorted_papers = sort_paper_by_n_genes_in_abstract(papers, genes, verbose=False)
+        
+        #title_matchings = check_title_matching(paper, paragraph, config, verbose=verbose) for paper in sorted_papers]
+        title_matching_papers = []
+        genes_to_be_searched = genes.copy()
+        for paper in sorted_papers:
+            genes_in_abstract = get_genes_in_abstract(paper, genes, verbose=False)
+            if len(genes_in_abstract)>1:
+                title_matching = check_title_matching(paper, paragraph, config, verbose=verbose)
+                if title_matching:
+                    title = paper['MedlineCitation']['Article']['ArticleTitle']
+                    abstract = paper['MedlineCitation']['Article']['Abstract']['AbstractText'][0]
+                    title_matching_papers.append(paper)
+                    for gene_in_abstract in genes_in_abstract:
+                        if gene_in_abstract in genes_to_be_searched:
+                            genes_to_be_searched.remove(gene_in_abstract)
+                    #print(title)
+        print("The number of title matching paper: %d"%len(title_matching_papers))
+        paper_for_references = []
+        genes_to_be_searched = genes.copy()
+        for paper in title_matching_papers:
+            if check_abstract_match(paper, paragraph, config, verbose=verbose):
+                genes_in_abstract = get_genes_in_abstract(paper, genes, verbose=False)
+                if len([ gene_in_abstract for gene_in_abstract in genes_in_abstract if gene_in_abstract in genes_to_be_searched])==0:
+                    continue
+                paper_for_references.append(paper)
+                for gene_in_abstract in genes_in_abstract:
+                    if gene_in_abstract in genes_to_be_searched:
+                        genes_to_be_searched.remove(gene_in_abstract)
+                        print(title)
+                if len(genes_to_be_searched)==0:
+                    break
+        references = [get_mla_citation_from_pubmed_id(paper) for paper in paper_for_references]
+        abstract_paragraph = ["\n".join(paper['MedlineCitation']['Article']['Abstract']['AbstractText']) for paper in paper_for_references]
         references_paragraphs.append(references) 
+        abstracts.append(abstract_paragraph)
         print("In paragraph %d, %d references are matched"%(i+1, len(references)))
         print("")
         print("")
@@ -320,7 +484,5 @@ def get_references_for_paragraphs(paragraphs, email, config, n=5, verbose=False,
             j+=1
         referenced_paragraphs += "\n\n"
         # referenced_paragraphs += "\n\nKeyword combinations: %s"%keyword_joined + '\n\n'
-
-            
-    return referenced_paragraphs + footer
+    return referenced_paragraphs + footer#, abstracts
 
