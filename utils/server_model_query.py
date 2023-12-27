5d1732f03b81e2e08b7112b769391e8b1153e321
Clara Hu <mhu@health.ucsd.edu>
2023-12-27 10:44:50 -0800

    handle errors for server model

--- utils/server_model_query.py
+++ utils/server_model_query.py
@@ -35,7 +35,7 @@ def server_model_chat(context, prompt, model,temperature, max_tokens, LOG_FILE, 
     }
     while retries <= max_retries: ## allow a max of 5 retries if the server is busy or overloaded
         try:
-            response = requests.post(url, json = data)
+            response = requests.post(url, json = data, timeout= 120)
 
             # Check if the request was successful
             if response.status_code == 200:
@@ -57,6 +57,14 @@ def server_model_chat(context, prompt, model,temperature, max_tokens, LOG_FILE, 
                 save_log(LOG_FILE,log_data) # save the log
                 
                 return  analysis
+            elif response.status_code == 503:
+                print('Encountering server issue. Retrying in ', backoff_time, ' seconds')
+                time.sleep(backoff_time)
+                retries += 1
+                backoff_time *= 2
+            else:
+                print('The request failed with status code: ', response.status_code)
+                return None
         except requests.exceptions.RequestException as e:
             print('The request failed with an exception: ', e, ' Retrying in ', backoff_time, ' seconds')
             time.sleep(backoff_time)
