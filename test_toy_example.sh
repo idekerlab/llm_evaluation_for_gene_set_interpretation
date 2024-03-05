3dcd122db26a6539615202cd9cc6657ea7915dd1
Clara Hu <mhu@health.ucsd.edu>
2024-03-04 19:48:45 -0800

    update task1 data

---
+++ test_toy_example.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#SBATCH --output=./outfile/gpt_go_analysis.%A_%a.out
+#SBATCH --error=./errfile/gpt_go_analysis.%A_%a.err
+#SBATCH --job-name=llm_go_analysis
+#SBATCH --partition="nrnb-compute" # replace with your partition name
+#SBATCH --array=6 # run specific array task
+#SBATCH --time=3:00:00
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=1
+
+hostname
+date
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+
+
+PARAMFILE=toy_example_params.txt
+
+PARAM=$(awk "NR==$SLURM_ARRAY_TASK_ID" $PARAMFILE)
+echo $PARAM
+
+source activate llm_eval
+# run llm query
+eval python query_llm_for_analysis.py $PARAM
+
+date
