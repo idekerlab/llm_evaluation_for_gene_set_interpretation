17effa01a7261472b89683451685578186cf58af
Clara Hu <mhu@health.ucsd.edu>
2024-03-04 17:50:16 -0800

    clean up and upload files for runing LLM query

--- thousandGOsets_GPT4Run.sh
+++ thousandGOsets_GPT4Run.sh
@@ -3,7 +3,7 @@
 #SBATCH --error=./errfile/gpt_go_analysis.%A_%a.err
 #SBATCH --job-name=llm_go_analysis
 #SBATCH --partition="nrnb-compute" # replace with your partition name
-#SBATCH --array=13 # run specific array task
+#SBATCH --array=39-40 # change if run specific array task
 #SBATCH --time=3:00:00
 #SBATCH --mem=8G
 #SBATCH --cpus-per-task=1
@@ -13,11 +13,10 @@ date
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 
 
-PARAMFILE=model_compare_params.txt
+PARAMFILE=thousandGOsets_CC_MF_GPT4Run_params.txt
 
 PARAM=$(awk "NR==$SLURM_ARRAY_TASK_ID" $PARAMFILE)
-echo $P
-sbaARAM
+echo $PARAM
 
 source activate llm_eval
 # run llm query
